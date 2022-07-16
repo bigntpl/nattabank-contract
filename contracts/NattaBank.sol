fbc471fcb497ebef1e04da646f06e77746290efc
Nattapon Lertkijroongreung <nattapon.ler@hotmail.com>
2022-07-16 22:43:11 +0700

    feat: add deposit feature

--- contracts/NattaBank.sol
+++ contracts/NattaBank.sol
@@ -9,10 +9,16 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeab
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
 
 contract NattaBank is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+  using SafeERC20Upgradeable for IERC20Upgradeable;
+
   // dev errors
   error NattaBank_NoExistedAccountNameIsAllowed();
+  error NattaBank_InvalidDepositingAmount();
+  error NattaBank_AccountNameNotFound();
+
   // dev events
   event CreateAccount(address caller, string accountName);
+  event Deposit(string accountName, uint256 amount);
 
   struct AccountInfo {
     string accountName;
@@ -20,9 +26,11 @@ contract NattaBank is OwnableUpgradeable, ReentrancyGuardUpgradeable {
   }
 
   IERC20Upgradeable public erc20Token;
+  uint256 public allBalance;
   mapping(address => AccountInfo[]) public accountInfo;
 
   /// @notice Upgradeable's initialization function
+  /// @param _erc20Token address of ERC20 token
   function initialize(address _erc20Token) external initializer {
     // Initialize the contract.
     // This function is called by the constructor.
@@ -32,11 +40,31 @@ contract NattaBank is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     erc20Token = IERC20Upgradeable(_erc20Token);
   }
 
+  /// @dev function for viewing length of accountInfo
   function getAccountLength() public view returns (uint256) {
     return accountInfo[msg.sender].length;
   }
 
-  function createAccount(string memory _accountName) external {
+  /// @dev function for retrieve account Id for accountName given
+  /// @param _accountName account name that wants to be retrieved
+  function getAccountId(string calldata _accountName)
+    public
+    view
+    returns (uint256)
+  {
+    uint256 i;
+    for (i = 0; i < getAccountLength(); i++) {
+      string memory accountName = accountInfo[msg.sender][i].accountName;
+      if (keccak256(bytes(accountName)) == keccak256(bytes(_accountName))) {
+        return i;
+      }
+    }
+    revert NattaBank_AccountNameNotFound();
+  }
+
+  /// @dev function for creating bank account
+  /// @param _accountName account name for creating bank account
+  function createAccount(string calldata _accountName) external {
     for (uint8 i = 0; i < getAccountLength(); i++) {
       string memory accountName = accountInfo[msg.sender][i].accountName;
       if (keccak256(bytes(accountName)) == keccak256(bytes(_accountName))) {
@@ -51,8 +79,24 @@ contract NattaBank is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     emit CreateAccount(msg.sender, _accountName);
   }
 
-  function deposit(uint256 _amount) external {
+  /// @dev function for deposit token to bank account
+  /// @param _amount amount to deposit
+  /// @param _accountName account name for depositing token
+  function deposit(uint256 _amount, string calldata _accountName)
+    external
+    nonReentrant
+  {
+    if (_amount == 0) {
+      revert NattaBank_InvalidDepositingAmount();
+    }
 
+    uint256 accountId = getAccountId(_accountName);
+    accountInfo[msg.sender][accountId].amount += _amount;
+    allBalance += _amount;
+
+    erc20Token.safeTransferFrom(msg.sender, address(this), _amount);
+
+    emit Deposit(_accountName, _amount);
   }
 
   function withdraw() external {}
