8471e11bb0182227bcbeff6f3eb8a34fb8e55f3b
bigntpl <nattapon.ler@hotmail.com>
2022-08-01 10:48:31 +0700

    chore: update dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('hardhat-contract-sizer');
 
 const config: HardhatUserConfig = {
   solidity: {
